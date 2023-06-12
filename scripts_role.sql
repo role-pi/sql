e0b3aec84c193e7ae5a0984dd7b2b91b20925198
b33 <111303609+fr0styb33@users.noreply.github.com>
2023-06-12 07:47:05 -0300

    Update scripts_role.sql

--- scripts_role.sql
+++ scripts_role.sql
@@ -300,18 +300,18 @@ UPDATE enderecos SET cep = 29212462, logradouro = 'Judy', bairro =  'Pierstorff'
 UPDATE enderecos SET cep = 29212462, logradouro = 'Judy', bairro = 'Pierstorff', cidade = 'Saint Augustine', estado = 'Florida', país = 'United States' WHERE id_cep = 18;
 
 --Evento
-UPDATE eventos SET (id_evento = 14, nome = 'Berg''s Hedgenettle', data_inicio = '2023-03-12 21:50:09', data_fim = '2023-03-19 21:44:29', enderecos_cep = 74593766, complemento_endereco = 'Parkway') WHERE id_evento = 5;
-UPDATE eventos SET (id_evento = 15, nome = 'Buddlejaleaf Viburnum', data_inicio =  '2022-10-25 22:07:36', data_fim = '2022-11-27 13:12:19', enderecos_cep = 74593766, complemento_endereco = 'Junction') WHERE id_evento = 14;
-UPDATE eventos SET (id_evento = 16, nome = 'Carolina Oatgrass', data_inicio = '2023-01-30 20:01:38', data_fim = '2022-12-09 06:47:49', enderecos_cep = 74593766, complemento_endereco = 'Trail') WHERE id_evento = 18;
-UPDATE eventos SET (id_evento = 17, nome = 'Hairy Nightshade', data_inicio = '2022-09-29 23:31:23', data_fim = '2022-10-26 01:43:16', enderecos_cep = 74593766, complemento_endereco = 'Hill') WHERE id_evento = 3;
-UPDATE eventos SET (id_evento = 18, nome = 'Cleft Phlox', data_inicio = '2022-08-09 03:02:11', data_fim = '2022-11-11 17:20:25', enderecos_cep = 74593766, complemento_endereco = 'Road') WHERE id_evento = 1;
-UPDATE eventos SET (id_evento = 19, nome = 'Ross'' Avens', data_inicio = '2022-07-04 14:21:32', data_fim = '2022-06-21 00:05:10', enderecos_cep = 74593766, complemento_endereco = 'Lane') WHERE id_evento = 5;
-UPDATE eventos SET (id_evento = 20, nome = 'Longleaf False Goldeneye', data_inicio = '2022-06-08 12:01:23', data_fim = '2022-08-17 15:58:16', enderecos_cep = 74593766, complemento_endereco = 'Court') WHERE id_evento = 5;
-UPDATE eventos SET (id_evento = 21, nome = 'Welsh''s Bugseed', data_inicio = '2022-06-03 16:55:02', data_fim = '2023-02-08 02:21:41', enderecos_cep = 74593766, complemento_endereco = 'Terrace') WHERE id_evento = 5;
-UPDATE eventos SET (id_evento = 22, nome = 'Red Grama', data_inicio = '2023-03-02 16:46:42', data_fim = '2022-06-05 00:40:39', enderecos_cep = 74593766, complemento_endereco = 'Parkway') WHERE id_evento = 14;
-UPDATE eventos SET (id_evento = 23, nome = 'Woolly Brickellbush', data_inicio = '2023-02-16 22:01:53', data_fim = '2022-07-12 07:09:38', enderecos_cep = 74593766, complemento_endereco = 'Hill') = 18;
-UPDATE eventos SET id_evento = 24, nome = 'Plagiobryum Moss', data_inicio = '2022-06-21 13:01:20', data_fim = '2022-06-24 11:16:10', enderecos_cep = 74593766, complemento_endereco = 'Road') WHERE id_evento = 14;
-UPDATE eventos SET id_evento = 25, nome = 'Henbane', data_inicio = '2023-04-19 22:30:47', data_fim = '2022-06-01 18:48:44', enderecos_cep = 74593766, complemento_endereco = 'Lane' WHERE id_evento = 18;
+UPDATE eventos SET nome = 'Berg''s Hedgenettle', data_inicio = '2023-03-12 21:50:09', data_fim = '2023-03-19 21:44:29', enderecos_cep = 74593766, complemento_endereco = 'Parkway' WHERE id_evento = 5;
+UPDATE eventos SET nome = 'Buddlejaleaf Viburnum', data_inicio =  '2022-10-25 22:07:36', data_fim = '2022-11-27 13:12:19', enderecos_cep = 74593766, complemento_endereco = 'Junction' WHERE id_evento = 14;
+UPDATE eventos SET nome = 'Carolina Oatgrass', data_inicio = '2023-01-30 20:01:38', data_fim = '2022-12-09 06:47:49', enderecos_cep = 74593766, complemento_endereco = 'Trail' WHERE id_evento = 18;
+UPDATE eventos SET nome = 'Hairy Nightshade', data_inicio = '2022-09-29 23:31:23', data_fim = '2022-10-26 01:43:16', enderecos_cep = 74593766, complemento_endereco = 'Hill' WHERE id_evento = 3;
+UPDATE eventos SET nome = 'Cleft Phlox', data_inicio = '2022-08-09 03:02:11', data_fim = '2022-11-11 17:20:25', enderecos_cep = 74593766, complemento_endereco = 'Road' WHERE id_evento = 1;
+UPDATE eventos SET nome = 'Ross'' Avens', data_inicio = '2022-07-04 14:21:32', data_fim = '2022-06-21 00:05:10', enderecos_cep = 74593766, complemento_endereco = 'Lane' WHERE id_evento = 5;
+UPDATE eventos SET nome = 'Longleaf False Goldeneye', data_inicio = '2022-06-08 12:01:23', data_fim = '2022-08-17 15:58:16', enderecos_cep = 74593766, complemento_endereco = 'Court' WHERE id_evento = 5;
+UPDATE eventos SET nome = 'Welsh''s Bugseed', data_inicio = '2022-06-03 16:55:02', data_fim = '2023-02-08 02:21:41', enderecos_cep = 74593766, complemento_endereco = 'Terrace' WHERE id_evento = 5;
+UPDATE eventos SET nome = 'Red Grama', data_inicio = '2023-03-02 16:46:42', data_fim = '2022-06-05 00:40:39', enderecos_cep = 74593766, complemento_endereco = 'Parkway' WHERE id_evento = 14;
+UPDATE eventos SET nome = 'Woolly Brickellbush', data_inicio = '2023-02-16 22:01:53', data_fim = '2022-07-12 07:09:38', enderecos_cep = 74593766, complemento_endereco = 'Hill' = 18;
+UPDATE eventos SET nome = 'Plagiobryum Moss', data_inicio = '2022-06-21 13:01:20', data_fim = '2022-06-24 11:16:10', enderecos_cep = 74593766, complemento_endereco = 'Road' WHERE id_evento = 14;
+UPDATE eventos SET nome = 'Henbane', data_inicio = '2023-04-19 22:30:47', data_fim = '2022-06-01 18:48:44', enderecos_cep = 74593766, complemento_endereco = 'Lane' WHERE id_evento = 18;
 
 --Transacoes
 UPDATE transacoes SET valor = 554.43, data = '2023-01-15 14:27:43' WHERE id_transacao = 1;
